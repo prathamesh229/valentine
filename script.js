12e0ffdd25280db28b46324463b16f8997a05f8b
prathamesh229 <prathupawar015@gmail.com>
2026-02-04 09:33:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function yesClick() {
-  alert("Yay Amruta 💖💫 Happy Valentine’s Day 🥰");
+  window.location.href = "yes.html";
 }
+
+function moveNo() {
+  const btn = document.getElementById("noBtn");
+  btn.style.position = "absolute";
+  btn.style.left = Math.random() * 80 + "vw";
+  btn.style.top = Math.random() * 80 + "vh";
+}
+
+/* Floating Emojis */
+const emojis = ["💖", "💕", "✨", "💘", "😍"];
+const container = document.querySelector(".emoji-container");
+
+setInterval(() => {
+  const emoji = document.createElement("span");
+  emoji.innerText = emojis[Math.floor(Math.random() * emojis.length)];
+  emoji.style.left = Math.random() * 100 + "vw";
+  emoji.style.animationDuration = (Math.random() * 3 + 4) + "s";
+  container.appendChild(emoji);
+
+  setTimeout(() => emoji.remove(), 6000);
+}, 500);
 
 function moveNo() {
   const btn = document.getElementById("noBtn");
