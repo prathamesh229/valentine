59a1ff24f84751c98187e9a17950c91232349f44
prathamesh229 <prathupawar015@gmail.com>
2026-02-04 09:46:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const yesBtn = document.getElementById("yesBtn");
-  const noBtn = document.getElementById("noBtn");
+function yesClick() {
+  alert("Yay! 💖 Happy Valentine’s Day 🥰");
+}
 
-  yesBtn.onclick = () => {
-    window.location.href = "yes.html";
-  };
-
-  noBtn.onmouseover = () => {
-    noBtn.style.position = "absolute";
-    noBtn.style.left = Math.random() * 80 + "vw";
-    noBtn.style.top = Math.random() * 80 + "vh";
-  };
-
-  const emojis = ["💖", "💕", "✨", "💘", "😍"];
-  const container = document.querySelector(".emoji-container");
-
-  setInterval(() => {
-    const emoji = document.createElement("span");
-    emoji.innerText = emojis[Math.floor(Math.random() * emojis.length)];
-    emoji.style.left = Math.random() * 100 + "vw";
-    emoji.style.animationDuration = (Math.random() * 3 + 4) + "s";
-    container.appendChild(emoji);
-    setTimeout(() => emoji.remove(), 6000);
-  }, 500);
-});
+function moveNo() {
+  const btn = document.getElementById("noBtn");
+  btn.style.position = "absolute";
+  btn.style.left = Math.random() * 80 + "vw";
+  btn.style.top = Math.random() * 80 + "vh";
+}
